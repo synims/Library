17c0bfa2a54e1fa5fb6cc1dd15aa03ccc399899a
Devendra Bharambe <devbharambe@gmail.com>
2015-12-07 08:31:43 +0530

    Library Testing

---
+++ Library/src/test/java/IMS/Library/Constants.java
@@ -0,0 +1,11 @@
+package IMS.Library;
+
+public class Constants {
+	// public static final String URL = "http://www.expedia.com";
+     public static final String File_Path = "E://Devendra//Syn//";
+     public static final String File_Name = "SynIMS.xlsx";
+     public static final String SheetN = "Appointment";
+     public static final String LibproLogin = "Login";
+     public static final String Library = "LibPro.xlsx";
+     public static final String Booklist = "BookList";
+}
